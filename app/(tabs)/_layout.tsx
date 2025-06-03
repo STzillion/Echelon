a065bad21bec69f7fc4406cf06b72e80e12f74ee
Samuel Mutia <mutiasamuel82@gmail.com>
2025-06-03 11:02:22 -0400

    plain expo project

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,24 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import Ionicons from '@expo/vector-icons/build/Ionicons';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#0a7ea4',
+        headerShown: false,
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name= {focused ?'home': 'home-outline'} color={color} size = {24} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
+
